c9023b7d8c8f7a4bacf3af4e67007e1470069607
Philip Bakken Holt-Seeland <philiph@netcompany.com>
2018-06-13 19:01:25 +0200

    Outline of app without redux.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import EksempelModule from "./module/eksempel-module";
+// import EksempelModule from "./module/eksempel-module";
+import Feed from "./module/components/Feed";
 
 class App extends Component {
   render() {
@@ -15,7 +16,10 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
 
-        <EksempelModule></EksempelModule>
+        {/*<EksempelModule></EksempelModule>*/}
+        <Feed title={"test"} description={"test desc"} clickEvent = {function(){alert("clicked")}}>
+
+        </Feed>
       </div>
     );
   }
---
+++ src/module/components/Feed.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../style/style.css';
+
+const Feed = ({ title, description, clickEvent }) => (
+  <div className="card">
+    <div className="container">
+      <span>
+        <h4><b>{title}</b></h4>
+        <p>{description}</p>
+        <button type="button" onClick={clickEvent}>Click Me!</button>
+      </span>
+    </div>
+  </div>
+);
+
+
+Feed.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+};
+
+Feed.defaultProps = {
+  title: '',
+  description: '',
+};
+
+export default Feed;
